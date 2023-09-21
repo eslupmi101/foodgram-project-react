3d1723b079b1839d98f978797f9f71d02bbc35c0
eslupmi101 <ajsen-andreev@yandex.ru>
2023-09-21 19:40:42 +0900

    Fix

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -6,57 +6,65 @@ from .models import (Favorite, Ingredient, Recipe, RecipeIngredient, RecipeTag,
 
 @admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
-    list_display = ('recipe', 'user')
-    list_filter = ('recipe', 'user')
-    search_fields = ('recipe__name', 'user__username')
+    list_display = ["recipe", "user"]
+    list_filter = ["recipe", "user"]
+    search_fields = ["recipe__name", "user__username"]
 
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'measurement_unit')
-    list_filter = ('name', 'measurement_unit')
-    search_fields = ('name', 'measurement_unit')
+    list_display = ["name", "measurement_unit"]
+    list_filter = ["name", "measurement_unit"]
+    search_fields = ["name", "measurement_unit"]
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'cooking_time', 'recipes_ingredients')
-    list_filter = ('name', 'author', 'cooking_time')
-    search_fields = ('name', 'author__username', 'cooking_time')
-    filter_horizontal = ('tags',)
+    list_display = [
+        "name", "author", "cooking_time",
+        "recipes_ingredients", "favorite_count"
+    ]
+    list_filter = ["name", "author", "tags"]
+    search_fields = ["name", "author__username", "cooking_time"]
+    filter_horizontal = ["tags"]
+
+    def favorite_count(self, obj):
+        return obj.favorites.count()
+
+    favorite_count.short_description = "Количество избранных"
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'color', 'slug')
-    list_filter = ('name', 'color', 'slug')
-    search_fields = ('name', 'color', 'slug')
+    list_display = ["name", "color", "slug"]
+    list_filter = ["name", "color", "slug"]
+    search_fields = ["name", "color", "slug"]
 
 
 @admin.register(RecipeIngredient)
 class RecipeIngredientAdmin(admin.ModelAdmin):
-    list_display = ('recipe', 'ingredient')
-    list_filter = ('recipe__name', 'ingredient__name')
-    search_fields = ('recipe__name', 'ingredient__name')
+    list_display = ["recipe", "ingredient"]
+    list_filter = ["recipe__name", "ingredient__name"]
+    search_fields = ["recipe__name", "ingredient__name"]
 
 
 @admin.register(RecipeTag)
 class RecipeTagAdmin(admin.ModelAdmin):
-    list_display = ('recipe', 'tag')
-    list_filter = ('recipe__name', 'tag__name')
-    search_fields = ('recipe__name', 'tag__name')
+    list_display = ["recipe", "tag"]
+    list_filter = ["recipe__name", "tag__name"]
+    search_fields = ["recipe__name", "tag__name"]
 
 
 @admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
-    list_display = ('id', 'owner')
-    list_filter = ('owner', 'recipes')
-    search_fields = ('owner__username',)
-    filter_horizontal = ('recipes',)
+    list_display = ["id", "owner"]
+    list_filter = ["owner", "recipes"]
+    search_fields = ["owner__username"]
+    filter_horizontal = ["recipes"]
 
 
 @admin.register(ShoppingCartRecipe)
 class ShoppingCartRecipeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'recipe', 'shopping_cart')
-    list_filter = ('recipe', 'shopping_cart')
-    search_fields = ('recipe__name', 'shopping_cart__owner__username')
+    list_display = ["id", "recipe", "shopping_cart"]
+    list_filter = ["recipe", "shopping_cart"]
+    search_fields = ["recipe__name", "shopping_cart__owner__username"]
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -7,31 +7,31 @@ from .models import Subscribe, User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ("username", "email", "first_name", "last_name", "is_staff")
-    list_filter = ("is_staff",)
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        (_("Personal Info"), {"fields": (
+    list_display = ["username", "email", "first_name", "last_name", "is_staff"]
+    list_filter = ["is_staff"]
+    fieldsets = [
+        [None, {"fields": ["email", "password"]}],
+        [_["Personal Info"], {"fields": [
             "username", "first_name", "last_name"
-        )}),
-        (_("Permissions"), {"fields": (
+        ]}],
+        [_["Permissions"], {"fields": [
             "is_active", "is_staff", "is_superuser",
             "groups", "user_permissions"
-        )}),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-    )
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("email", "password1", "password2"),
-        }),
-    )
-    search_fields = ("email", "username", "first_name", "last_name")
-    ordering = ("email",)
+        ]}],
+        [_["Important dates"], {"fields": ["last_login", "date_joined"]}],
+    ]
+    add_fieldsets = [
+        [None, {
+            "classes": ["wide"],
+            "fields": ["email", "password1", "password2"],
+        }],
+    ]
+    search_fields = ["email", "username", "first_name", "last_name"]
+    ordering = ["email"]
 
 
 @admin.register(Subscribe)
 class SubscribeAdmin(admin.ModelAdmin):
-    list_display = ("author", "subscriber")
-    list_filter = ("author", "subscriber")
-    search_fields = ("author__username", "subscriber__username")
+    list_display = ["author", "subscriber"]
+    list_filter = ["author", "subscriber"]
+    search_fields = ["author__username", "subscriber__username"]
