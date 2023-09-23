0c74f9359654615440b0fad22756ea8de16b7d44
eslupmi101 <ajsen-andreev@yandex.ru>
2023-09-23 23:42:02 +0900

    Fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -74,7 +74,7 @@ class UserViewSet(ModelViewSet):
         """
         authors = get_list_or_404(
             User,
-            subscribe_subscribers__subscriber=request.user,
+            subscribe_authors__subscriber=request.user,
         )
         serializer = serializers.UserRecipeSerializer(
             authors,
