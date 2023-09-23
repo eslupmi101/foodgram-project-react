0c37ab039526c7c64126594c394e3c7fc221792e
eslupmi101 <ajsen-andreev@yandex.ru>
2023-09-23 23:28:31 +0900

    Fix

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 
-from users.models import Subscribe
-
 
 class IsSubscribedUserSerializer(serializers.ModelSerializer):
     is_subscribed = serializers.SerializerMethodField()
