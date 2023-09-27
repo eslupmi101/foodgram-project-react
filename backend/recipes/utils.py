7d7f236e70fce24e6e653dbb0558799c7ab6fe7b
eslupmi101 <ajsen-andreev@yandex.ru>
2023-09-27 23:19:55 +0900

    Fix

--- backend/recipes/utils.py
+++ backend/recipes/utils.py
@@ -1,7 +1,9 @@
 import xlwt
 
+from recipes.models import Recipe
 
-def get_xls_recipes_file(recipes) -> xlwt.Workbook:
+
+def get_xls_recipes_file(recipes: list[Recipe]) -> xlwt.Workbook:
     """
     Возвращает xls workbook.
 
@@ -11,7 +13,10 @@ def get_xls_recipes_file(recipes) -> xlwt.Workbook:
     ws = wb.add_sheet("Recipes")
 
     row_num = 0
-    columns = ["Название", "Автор", "Описание", "Время приготовления"]
+    columns = [
+        "Номер", "Ингредиент", "Мера измерение", "Количество ингредиента",
+        "Название рецепта", "Автор", "Описание", "Время приготовления"
+    ]
     font_style = xlwt.XFStyle()
     font_style.font.bold = True
 
@@ -20,15 +25,24 @@ def get_xls_recipes_file(recipes) -> xlwt.Workbook:
 
     font_style = xlwt.XFStyle()
 
-    for recipe in recipes.all():
-        row_num += 1
-        row = [
-            recipe.name,
-            recipe.author.username,
-            recipe.text,
-            recipe.cooking_time
-        ]
-        for col_num, cell_value in enumerate(row):
-            ws.write(row_num, col_num, cell_value, font_style)
+    for recipe in recipes:
+        number_ingredient = 0
+
+        for recipe_ingredient in recipe.recipes_ingredients.all():
+            row_num += 1
+            row = [
+                str(number_ingredient),
+                recipe_ingredient.indredient.name,
+                recipe_ingredient.indredient.measurement_unit,
+                recipe_ingredient.amount,
+                recipe.name,
+                recipe.author.username,
+                recipe.text,
+                recipe.cooking_time
+            ]
+            number_ingredient += 1
+
+            for col_num, cell_value in enumerate(row):
+                ws.write(row_num, col_num, cell_value, font_style)
 
     return wb
