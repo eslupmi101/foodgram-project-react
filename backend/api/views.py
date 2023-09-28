3c9f7d0b28dd4a3b192466ed3ab88a99f00c4fe8
eslupmi101 <ajsen-andreev@yandex.ru>
2023-09-28 17:30:29 +0900

    Fast test commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-from django.db.models import BooleanField, Case, Value, When 
+from django.db.models import BooleanField, Case, Value, When
 from django.http import HttpResponse
 from django.shortcuts import get_list_or_404, get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
