ab18ceeee46b43d8060c7bb93cbefeede3838e13
eslupmi101 <ajsen-andreev@yandex.ru>
2023-09-28 20:39:27 +0900

    Fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -135,15 +135,6 @@ class RecipeViewSet(ModelViewSet):
                     output_field=BooleanField()
                 )
             )
-        else:
-            queryset = queryset.annotate(
-                is_favorited=Value(
-                    False, output_field=BooleanField()
-                ),
-                is_in_shopping_cart=Value(
-                    False, output_field=BooleanField()
-                )
-            )
 
         return queryset
 
